b9830d29de1ddec07e808b1241d05fe7d9aa75ae
Julian Diaz <114930613+Voladine@users.noreply.github.com>
2023-04-16 00:13:58 -0400

    Add functioning UI to the game

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const playerSelection = ""
 const computerSelection = ""
+const rock = document.querySelector(".rockButton")
+const paper = document.querySelector(".paperButton")
+const scissors = document.querySelector(".scissorsButton")
+const results = document.querySelector(".results")
+const winner = document.querySelector(".winner")
 let playerScore = 0
 let computerScore = 0
+let playerScoreText = document.querySelector(".playerScoreText")
+let computerScoreText = document.querySelector(".computerScoreText")
+
+// button event listeners
+rock.addEventListener("click", () => playRound('rock', computerSelection))
+paper.addEventListener("click", () => playRound('paper', computerSelection))
+scissors.addEventListener("click", () => playRound('scissors', computerSelection))
 
 // generate random computer choice
 function getComputerChoice() {
@@ -13,63 +24,62 @@ function getComputerChoice() {
 // win/lose conditionals for one round
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = prompt('Rock, Paper, or Scissors?')
     computerSelection = getComputerChoice()
+    winner.textContent = ""
 
         if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper' ) {
             computerScore += 1
-            console.log("You Lose! Paper beats Rock")
+            results.textContent = "You Lose! Paper beats Rock"
         }
         else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors' ) {
             playerScore += 1
-            console.log("You Win! Rock beats Scissors")
+            results.textContent = "You Win! Rock beats Scissors"
         }
         else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'rock' ) {
-            console.log("It's a Tie!")
+            results.textContent = "It's a Tie!"
         }
         else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock' ) {
             playerScore += 1
-            console.log("You Win! Paper beats Rock")
+            results.textContent = "You Win! Paper beats Rock"
         }
         else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors' ) {
             computerScore += 1
-            console.log("You Lose! Scissors beats Paper")
+            results.textContent = "You Lose! Scissors beats Paper"
         }
         else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'paper' ) {
-            console.log("It's a Tie!")
+            results.textContent = "It's a Tie!"
         }
         else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock' ) {
             computerScore += 1
-            console.log("You Lose! Rock beats Scissors")
+            results.textContent = "You Lose! Rock beats Scissors"
         }
         else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper' ) {
             playerScore += 1
-            console.log("You Win! Scissors beats Paper")
+            results.textContent = "You Win! Scissors beats Paper"
         }
         else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'scissors' ) {
-            console.log("It's a Tie!")
+            results.textContent = "It's a Tie!"
         }
         else {
-            console.log("You must enter Rock, Paper, or Scissors to play the game.")
+            results.textContent = "You must enter Rock, Paper, or Scissors to play the game."
         }
+
+        playerScoreText.textContent = playerScore
+        computerScoreText.textContent = computerScore
+
+        if (playerScore === 5) {
+            winner.textContent = `You won ${playerScore} to ${computerScore}!`
+            playerScore = 0
+            computerScore = 0
+            playerScoreText.textContent = playerScore
+            computerScoreText.textContent = computerScore
+        }
+        if (computerScore === 5) {
+            winner.textContent = `You lost ${playerScore} to ${computerScore}!`
+            playerScore = 0
+            computerScore = 0
+            playerScoreText.textContent = playerScore
+            computerScoreText.textContent = computerScore
+        }
+        
 }
-
-// game of five rounds
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection)
-        console.log(`Your score: ${playerScore}, Computer score: ${computerScore}`)
-    }
-
-    if (playerScore > computerScore) {
-        console.log(`You won ${playerScore} to ${computerScore}!`)
-    }
-    else if (playerScore < computerScore) {
-        console.log(`You lost ${playerScore} to ${computerScore}!`)
-    }
-    else {
-        console.log(`You tied ${playerScore} to ${computerScore}!`)
-    }
-}
-
-game()
